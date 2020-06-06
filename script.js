13674e8700e9e9a9fbde336cea5a0cd2e1f514d0
Saklain1704032 <61911332+Saklain1704032@users.noreply.github.com>
2020-06-06 15:49:01 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+(function (window) {
+  var haha = ["Yaakov", "Jhj", "Jeny", "Jason", "Petesen", "Frank", "Larry", "Paula"];
+
+  for (var i in haha) {
+    var firstLetter = haha[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(haha[i]);
+    } else {
+      helloSpeaker.speak(haha[i]);
+    }
+  }
+})(window);
